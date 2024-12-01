5b9227b54d17b8de152c5aa5a4bac618a221cead
Mara <marekdarek646@gmail.com>
2024-12-01 13:29:50 +0100

    day 1

--- day_1.py
+++ day_1.py
@@ -2,16 +2,14 @@ with open('cisla', "r") as file:
     cisla = file.read()  # Načte celý obsah souboru jako jeden řetězec
     #print(cisla)
 
-    levy = []  # Pole pro levé čísla
-    pravy = []  # Pole pro pravé čísla
+    levy = []
+    pravy = []
 
-    # Rozdělíme řetězec na části podle mezer (pokud je více mezer, použijeme split bez argumentu)
     cisla_split = cisla.split()  # Split dle jakýchkoli bílých znaků
 
-    # Předpokládáme, že každý pár čísel je na sudém a lichém indexu
     for i in range(len(cisla_split)):
         if i % 2 == 0:  # Sudé indexy pro levé číslo
-            levy.append(int(cisla_split[i]))  # Přidáme číslo do seznamu (jako integer)
+            levy.append(int(cisla_split[i]))
         else:  # Liché indexy pro pravé číslo
             pravy.append(int(cisla_split[i]))
 
@@ -25,12 +23,12 @@ nejmensi_cislo2 = sorted(pravy)
 print("Seřazená levá čísla:", nejmensi_cislo1)
 print("Seřazená pravá čísla:", nejmensi_cislo2)
 
-# Vypočítáme celkový rozdíl mezi čísly
+
 celkovy_rozdil = 0
 for i in range(min(len(nejmensi_cislo1), len(nejmensi_cislo2))):
-    rozdil = abs(nejmensi_cislo1[i] - nejmensi_cislo2[i])  # Rozdíl mezi čísly
+    rozdil = abs(nejmensi_cislo1[i] - nejmensi_cislo2[i])
     print(f"Rozdíl na indexu {i}: {rozdil}")
-    celkovy_rozdil += rozdil  # Sčítání celkového rozdílu
+    celkovy_rozdil += rozdil
 
 print(f"Celkový rozdíl: {celkovy_rozdil}")
 celkovy_rozdil2 = 0
@@ -38,7 +36,7 @@ for cislo in levy:
     pocet_vyskytu = pravy.count(cislo)
     #print(f"Číslo {cislo} se v seznamu pravy vyskytuje {pocet_vyskytu}krát.")
     if pocet_vyskytu > 0:
-        # Vypočítáme součin čísla a jeho počtu výskytů
+
         vysledek = cislo * pocet_vyskytu
         print(f"Součin {cislo} * {pocet_vyskytu} = {vysledek}")
         celkovy_rozdil2 += vysledek
